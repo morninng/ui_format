f8acd746499785ea578ed5b3cba4949cd9931f6a
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-27 20:12:28 +0900

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiFormatApp
+ * @description
+ * # uiFormatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiFormatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uiFormatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uiFormatApp
+ */
+angular.module('uiFormatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
