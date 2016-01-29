a7dfb53b8afdf312a5aee9bd7066e0d527464b04
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-29 22:46:36 +0900

    tab for article speech and article argument

--- app/scripts/controllers/article_layout.js
+++ app/scripts/controllers/article_layout.js
@@ -5,6 +5,8 @@ angular.module('uiFormatApp')
 
   	$scope.name = "kk";
     $scope.game_id = $stateParams.game_id;
+    $scope.argument_tab = "active_tab";
+    $scope.audio_transcript_tab = "deactive_tab";
     console.log("layout id" + $scope.game_id);
 
 
